f711ec5aeba79992266e908b39aae8d9d662db1a
Anup Khandelwal <anup4khandelwal@gmail.com>
2020-04-18 22:54:56 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -10,15 +10,16 @@ function reverse(str) {
   return str.split('').reduce((acc, char) => char + acc ,'')
 }
 
-module.exports = reverse;
-// function reverse(str) {
-//   return str.split('').reverse().join('');
-//}
+function reverse(str) {
+   return str.split('').reverse().join('');
+}
 
-// function reverse(str) {
-//   let reversed = '';
-//   for (let character of str) {
-//     reversed = character + reversed; 
-//   }
-//   return reversed;
-// }
+function reverse(str) {
+   let reversed = '';
+   for (let character of str) {
+     reversed = character + reversed; 
+   }
+   return reversed;
+}
+
+module.exports = reverse;
