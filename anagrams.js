39a9541a9bc6a16eb12f3005d711aaa216837450
Anup Khandelwal <anup4khandelwal@gmail.com>
2020-04-21 17:31:01 +0530

    Update anagrams.js

--- anagrams.js
+++ anagrams.js
@@ -22,11 +22,11 @@ function anagrams(stringA, stringB) {
   const aCharMap =  buildCharMap(stringA);
   const bCharMap = buildCharMap(stringB);
   if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-		  return false;
+      return false;
   }
   for (let char in aCharMap) {
     if(aCharMap[char] !== bCharMap[char]) {
-			return false;
+       return false;
     }
   }
   return true;
@@ -35,7 +35,7 @@ function buildCharMap(str) {
   const charMap = {};
   for(let char of str.replace(/[^\w]/g,'').toLowerCase()) {
     charMap[char] = charMap[char] +1 || 1;
-	}
+  }
   return charMap;
 }
 
