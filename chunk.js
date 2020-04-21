087934db9570ec58e619e164e203c34e06d1c272
Anup Khandelwal <anup4khandelwal@gmail.com>
2020-04-21 17:38:17 +0530

    Array Chunking

--- chunk.js
+++ chunk.js
@@ -25,7 +25,7 @@ function chunk(array, size) {
   const chunked = [];
   let index = 0;
   while(index < array.length) {
- 	  chunked.push(array.slice(index, index + size))
+    chunked.push(array.slice(index, index + size));
     index += size; 
   }
   return chunked;
